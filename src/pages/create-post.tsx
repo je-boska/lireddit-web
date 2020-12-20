f8bff88b34d243e38118aaf6d3204be93b2e8352
joneirikboska <joneirikboska@gmail.com>
2020-12-20 17:09:18 +0100

    Send user to login if logged out on create post page. Add create post link to index.

--- src/pages/create-post.tsx
+++ src/pages/create-post.tsx
@@ -1,15 +1,23 @@
 import { Box, Button } from '@chakra-ui/react'
 import { Formik, Form } from 'formik'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { InputField } from '../components/InputField'
-import { useCreatePostMutation } from '../generated/graphql'
+import { useCreatePostMutation, useMeQuery } from '../generated/graphql'
 import { useRouter } from 'next/router'
 import { withUrqlClient } from 'next-urql'
 import { createUrqlClient } from '../utils/createUrqlClient'
 import { Layout } from '../components/Layout'
 
 const CreatePost: React.FC = ({}) => {
+  const [{ data, fetching }] = useMeQuery()
   const router = useRouter()
+
+  useEffect(() => {
+    if (!fetching && !data?.me) {
+      router.replace('/login')
+    }
+  }, [fetching, data, router])
+
   const [, createPost] = useCreatePostMutation()
 
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,24 @@
-import { NavBar } from '../components/NavBar'
 import { withUrqlClient } from 'next-urql'
 import { createUrqlClient } from '../utils/createUrqlClient'
 import { usePostsQuery } from '../generated/graphql'
+import { Layout } from '../components/Layout'
+import NextLink from 'next/link'
+import { Link } from '@chakra-ui/react'
 
 const Index = () => {
   const [{ data }] = usePostsQuery()
   return (
-    <>
-      <NavBar />
-      <div>Hello world</div>
+    <Layout>
+      <NextLink href='/create-post'>
+        <Link>Create post</Link>
+      </NextLink>
       <br />
       {!data ? (
         <div>loading...</div>
       ) : (
         data.posts.map(post => <div key={post.id}>{post.title}</div>)
       )}
-    </>
+    </Layout>
   )
 }
 
