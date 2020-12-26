b2d69fbf90ca7be67a10a48de16ae8c63d51de19
joneirikboska <joneirikboska@gmail.com>
2020-12-26 16:30:16 +0100

    Wrap posts in a stack.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,37 @@
+import React from 'react'
 import { withUrqlClient } from 'next-urql'
 import { createUrqlClient } from '../utils/createUrqlClient'
 import { usePostsQuery } from '../generated/graphql'
 import { Layout } from '../components/Layout'
 import NextLink from 'next/link'
-import { Link } from '@chakra-ui/react'
+import { Box, Flex, Heading, Link, Stack, Text } from '@chakra-ui/react'
 
 const Index = () => {
-  const [{ data }] = usePostsQuery()
+  const [{ data }] = usePostsQuery({
+    variables: {
+      limit: 10,
+    },
+  })
   return (
     <Layout>
-      <NextLink href='/create-post'>
-        <Link>Create post</Link>
-      </NextLink>
+      <Flex align='center'>
+        <Heading>LiReddit</Heading>
+        <NextLink href='/create-post'>
+          <Link ml={'auto'}>Create post</Link>
+        </NextLink>
+      </Flex>
       <br />
       {!data ? (
         <div>loading...</div>
       ) : (
-        data.posts.map(post => <div key={post.id}>{post.title}</div>)
+        <Stack spacing={8}>
+          {data.posts.map(post => (
+            <Box key={post.id} shadow='md' borderWidth='1px' padding='20px'>
+              <Heading fontSize='xl'>{post.title}</Heading>
+              <Text mt={4}>{post.textSnippet}</Text>
+            </Box>
+          ))}
+        </Stack>
       )}
     </Layout>
   )
